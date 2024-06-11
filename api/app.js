a2b7ce2340ee987bf11e20da3e6da6938ea39ea5
adibmannan1 <adibmannan1@gmail.com>
2024-06-11 09:43:02 +0600

    frontend deployment

--- api/app.js
+++ api/app.js
@@ -12,7 +12,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
   // origin: process.env.CLIENT_URL, 
-  origin: ["https://skyline-flame.vercel.app", process.env.CLIENT_URL], 
+  origin: ["https://skyline-estates.vercel.app/", process.env.CLIENT_URL], 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }))
